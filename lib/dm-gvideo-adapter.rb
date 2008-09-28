7303d6f6c74291f685166d35f371e93f343cd0bf
Matt Aimonetti <mattaimonetti@gmail.com>
2008-09-28 13:14:19 -0700

    getting started with the adapter methods

---
+++ lib/dm-gvideo-adapter.rb
@@ -0,0 +1,27 @@
+begin 
+  require 'minigems'
+rescue LoadError 
+  require 'rubygems'
+end
+require 'dm-core'
+require 'hpricot'
+require 'open-uri'
+
+module DataMapper
+  module Adapters
+    class GvideoAdapter < AbstractAdapter
+      def initialize(name, uri_or_options)
+        super(name, uri_or_options)
+        @resource_naming_convention = NamingConventions::Resource::Underscored
+      end
+
+      # Reads in a set from a query.
+      def read_many(query)
+      end
+
+      def read_one(query)
+      end
+      
+    end #GvideoAdapter
+  end #Adapters
+end #DataMapper
